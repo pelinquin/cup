abf1c77317ead2800d2d6456e8107056593b9e51
pelinquin <pelinquin@gmail.com>
2013-02-01 21:26:28 +0100

    frontpage

--- bank.py
+++ bank.py
@@ -26,20 +26,116 @@ from Crypto.Cipher import AES
 __digest__ = base64.urlsafe_b64encode(hashlib.sha1(open(__file__, 'r', encoding='utf-8').read().encode('utf-8')).digest())[:5]
 
 RSA_E = 65537
-MAX_ARG_SIZE = 850
+MAX_ARG_SIZE = 2000
 
 def reg(value):
     " function attribute is a way to access matching group in one line test "
     reg.v = value
     return value
 
+def init_db(db):
+    "_"
+    if not os.path.isfile(db):
+        d = dbm.open(db[:-3], 'c')
+        d['__DIGEST__'], d['NB_TR'] = __digest__, '0'
+        d.close()
+
 def application(environ, start_response):
+    "wsgi server app"
+    mime, o, db, today = 'text/plain; charset=utf-8', 'Error:', '/u/bank.db', '%s' % datetime.datetime.now()
+    init_db(db)
+    if environ['REQUEST_METHOD'].lower() == 'post':
+        raw, way = urllib.parse.unquote(environ['wsgi.input'].read().decode('utf-8')), 'post'
+    else:
+        raw, way = urllib.parse.unquote(environ['QUERY_STRING']), 'get'
+    d = dbm.open(db[:-3], 'c')
+    if len(raw) < MAX_ARG_SIZE:
+        if reg(re.match(r'^\s*TEST\s*(\(\s*"([^"]{3,})"\s*\))\s*$', raw, re.U)):
+            a = eval(reg.v.group(1))
+            assert (a == reg.v.group(2))
+            o = '1 ARG %s %s' % (way, raw)
+        elif reg(re.match(r'^\s*TEST\s*(\(\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*\))\s*$', raw, re.U)):
+            a = eval(reg.v.group(1))
+            assert (a[0] == reg.v.group(2) and a[1] == reg.v.group(3)) 
+            o = '2 ARGS %s %s' % (way, raw)
+        elif reg(re.match(r'^\s*TEST\s*(\(\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*\))\s*$', raw, re.U)):
+            a = eval(reg.v.group(1))
+            assert (a[0] == reg.v.group(2) and a[1] == reg.v.group(3) and a[2] == reg.v.group(4)) 
+            o = '3 ARGS %s %s' % (way, raw)
+        elif reg(re.match(r'^\s*(reg|register)\s*(\(\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*,\s*"([^"]{3,})"\s*\))\s*$', raw, re.U)):
+            c, a = reg.v.group(1).lower(), eval(reg.v.group(2))
+            assert (a[0] == reg.v.group(3) and a[1] == reg.v.group(4) and a[2] == reg.v.group(5) and a[3] == reg.v.group(6)) 
+            #o = '4 ARGS %s %s %s' % (c, way, raw)
+            pk, bal, ovr = b'PUB_' + bytes(a[0], 'utf8'), b'BAL_' + bytes(a[0], 'utf8'), b'OVR_' + bytes(a[0], 'utf8')
+            if pk in d.keys():
+                o += 'Public key already set!' 
+            elif verify(RSA_E, b64toi(bytes(a[2], 'ascii')), ' '.join((today[:10], a[0], a[1])), bytes(a[3], 'ascii')):
+                d[pk], d[bal], d[ovr], o = a[1], '0', '100', 'Public key id registration OK'
+            else:
+                o += 'Error in registration!'
+        elif way == 'get':
+            o, mime = frontpage(today), 'application/xhtml+xml; charset=utf-8'
+    else:
+        o += 'arg too long'
+    d.close()
+    start_response('200 OK', [('Content-type', mime)])
+    return [o.encode('utf-8')] 
+
+def frontpage(today):
+    "_"
+    o = '<?xml version="1.0" encoding="utf-8"?>\n' 
+    o += '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">\n'
+    o += '<link rel="shortcut icon" type="image/png" href="favicon.png"/>\n'
+    o += '<style>@import url(http://fonts.googleapis.com/css?family=Schoolbell);</style>\n'
+    o += '<style type="text/css">text,path{stroke:none;fill:Dodgerblue;font-family:helvetica;}text.foot{fill:gray;text-anchor:start;}text.alpha{font-family:Schoolbell;fill:#F87217;}text.note{fill:#CCC;font-size:9pt;text-anchor:end;}text.w{fill:white;}rect{fill:#CCC;}</style>\n'
+    o += '<a xlink:href="http://cupfoundation.net"><path stroke-width="0" d="M10,10L10,10L10,70L70,70L70,10L60,10L60,60L20,60L20,10z"/></a>'
+    d = dbm.open('/u/bank')
+    nb, su, ck = 0, 0, 0
+    for x in d.keys():
+        if reg(re.match('BAL_(.*)$', x.decode('utf-8'))):
+            nb += 1
+            su += abs(float(d[x])/2)
+            ck += float(d[x])
+    o += '<text x="90" y="70" font-size="48" title="banking for intangible goods">Bank</text>\n'
+    o += '<text class="alpha" font-size="14pt" x="72"  y="25" title="still in security test!">Beta</text>\n'
+    o += '<text class="alpha" font-size="64pt" text-anchor="middle" x="50%" y="40%"><tspan>No \"https\", no JavaScript,</tspan><tspan x="50%" dy="100">better security!</tspan></text>\n'
+    o += '<text class="foot" x="20"  y="100">%s</text>\n' % today[:19]
+    o += '<text class="foot" x="20%%"  y="80%%">%04d users</text>\n' % nb
+    o += '<text class="foot" x="40%%" y="80%%">%05d transactions</text>\n' % int(d['NB_TR'])
+    o += '<text class="foot" x="60%%" y="80%%">volume: %09.2f ⊔</text>\n' % su
+    o += '<text class="foot" x="80%%" y="80%%" title="number of registered Intangible Goods">%04d IG</text>\n' % (0)
+    o += '<a xlink:href="bank?source"><text class="note" text-anchor="start" x="160" y="98%" title="hack it, share it!">Download the source code!</text></a>\n'
+    o += '<a xlink:href="u?pi"><text class="note" x="99%" y="20" title="at home!">Host</text></a>\n'            
+    o += '<a xlink:href="bank?log"><text class="note" x="99%" y="40">Log</text></a>\n'
+    o += '<a xlink:href="bank?help"><text class="note" x="99%" y="60">Help</text></a>\n'
+    o += '<text class="note" x="99%%" y="92%%">Status: %s</text>\n' % ('ok' if (abs(ck) <= 0.00001) else 'error')
+    o += '<text class="note" x="99%%" y="95%%">Digest: %s|%s</text>\n' % (d['__DIGEST__'].decode('utf-8'), __digest__.decode('utf-8'))
+    o += '<text class="note" x="99%"  y="98%" title="or visit \'www.cupfoundation.net\'">Contact: laurent.fournier@cupfoundation.net</text>\n' 
+    d.close()
+    return o + '</svg>\n'
+
+def register(owner, iduser='anonymous', post=False, host='localhost'):
+    co, td = http.client.HTTPConnection(host), '%s' % datetime.datetime.now()
+    ds = dbm.open('/u/sk')    
+    ki, kb = [b64toi(x) for x in ds[owner].split()], [x for x in ds[owner].split()]
+    ds.close()
+    s = sign(ki[1], ki[2], ' '.join((td[:10], owner, iduser)))
+    cmd = 'reg("%s","%s","%s","%s")' % (owner, iduser, kb[1].decode('ascii'), s.decode('ascii'))
+    if post:
+        co.request('POST', '/bank', urllib.parse.quote(cmd))
+    else:
+        co.request('GET', '/bank?' + urllib.parse.quote(cmd))
+    #print('http://%s/bank?' % host + urllib.parse.quote(cmd))
+    return co.getresponse().read().decode('utf-8')
+            
+def application_old(environ, start_response):
     "wsgi server app"
     mime = 'text/plain; charset=utf-8'
     if not os.path.isfile('/u/bank.db'):
         d = dbm.open('/u/bank', 'c')
         d['__DIGEST__'], d['NB_TR'] = __digest__, '0'
         d.close()
+
     if environ['REQUEST_METHOD'].lower() == 'post':
         today = '%s' % datetime.datetime.now()
         raw, o = urllib.parse.unquote(environ['wsgi.input'].read().decode('utf-8')), 'error!'
@@ -97,7 +193,7 @@ def application(environ, start_response):
                     o = 'Verrif fail in IG registration!'
             else:
                 o = 'Error in IG registration!'
-        # READ BALLANCE
+        # READ BALANCE
         elif reg(re.match(r'^BAL(\(\s*"([^\"]{3,30})".*)$', raw, re.U)): 
             t, name = eval(reg.v.group(1)), b'BAL_' + bytes(reg.v.group(2), 'utf8')
             assert (t[0] == reg.v.group(2))
@@ -105,7 +201,7 @@ def application(environ, start_response):
                 pk = bytes('PUB_%s' % t[0], 'utf-8')
                 if verify(RSA_E, b64toi(d[pk]), today[:10], t[2]):
                     if t[1]:
-                        o, a = '%s ballance: %s ⊔\n' % (today, d[name].decode('utf-8')), []
+                        o, a = '%s balance: %s ⊔\n' % (today, d[name].decode('utf-8')), []
                         for x in d.keys():
                             if reg(re.match('(\d{4}.*)$', x.decode('utf-8'))):
                                 tab = d[x].split()
@@ -153,13 +249,11 @@ def application(environ, start_response):
             o = 'Welcome to ⊔net!\n\n'
             o += 'First you need your own 4096 bits long RSA key (used GPG or sshgen to generate it)\n\n'
             o += '! You will never have to send us your private key; keep it secure, out of Internet access\n\n'
-
             o += 'The main integrity rule is that the sum of all acounts balance is always null.\n\n'
-
             o += 'HTTP POST only request:\n'
             o += '\tPK(agent, public_key)\n'
             o += '\tTR(buyer, seller, price, current_date, buyer_signature) with signed message = \'seller|price|\' returns status (OK,KO)\n'
-            o += '\tBAL(owner, owner_signature) with signed message = \'date_of_the_day\' returns ballance\n'
+            o += '\tBAL(owner, owner_signature) with signed message = \'date_of_the_day\' returns balance\n'
             o += 'HTTP GET request:\n\tstat\n\tsource\n\tlog\n'
         else:
             o = '<?xml version="1.0" encoding="utf-8"?>\n' 
@@ -168,7 +262,7 @@ def application(environ, start_response):
             #o += '<link xlink:href="http://fonts.googleapis.com/css?family=Schoolbell" rel="stylesheet" type="text/css"/>\n' 
             o += '<style>@import url(http://fonts.googleapis.com/css?family=Schoolbell);</style>\n'
             o += '<style type="text/css">text,path{stroke:none;fill:Dodgerblue;font-family:helvetica;}text.foot{fill:gray;}text.alpha{font-family:Schoolbell;fill:#F87217;}text.note{fill:#CCC;font-size:8pt;text-anchor:end;}text.w{fill:white;}rect{fill:#CCC;}</style>\n'
-            o += '<path stroke-width="0" d="M10,10L10,10L10,70L70,70L70,10L60,10L60,60L20,60L20,10z"/>'
+            o += '<a xlink:href="http://cupfoundation.net"><path stroke-width="0" d="M10,10L10,10L10,70L70,70L70,10L60,10L60,60L20,60L20,10z"/></a>'
             d = dbm.open('/u/bank',)
             nb, su, ck = 0, 0, 0
             for x in d.keys():
@@ -179,20 +273,20 @@ def application(environ, start_response):
             today = '%s' % datetime.datetime.now()
             o += '<text x="90" y="70" font-size="48" title="banking for intangible goods">Bank</text>\n'
             o += '<text class="alpha" font-size="14pt" x="72"  y="25" title="still in security test!">Beta</text>\n'
-            o += '<text class="alpha" font-size="50pt" x="100" y="180">No \"https\", better security!</text>\n'
+            o += '<text class="alpha" font-size="64pt" text-anchor="middle" x="50%" y="50%"><span>No \"https\", no JavaScript,</span><span>better security!</span></text>\n'
 
             o += '<text class="foot" x="20"  y="100">%s</text>\n' % today[:19]
-            o += '<text class="foot" x="20"  y="260">%04d users</text>\n' % nb
-            o += '<text class="foot" x="120" y="260">%05d transactions</text>\n' % int(d['NB_TR'])
-            o += '<text class="foot" x="280" y="260">volume: %7.3f ⊔</text>\n' % su
-            o += '<text class="foot" x="480" y="260" title="number of registered Intangible Goods">%04d IG</text>\n' % (0)
-            o += '<a xlink:href="bank?source"><rect x="6" y="95%" width="210" height="25" rx="8" ry="8"/><text class="w" x="10" y="98%" title="hack it, share it!">Download the source code!</text></a>\n'
+            o += '<text class="foot" x="20"  y="360">%04d users</text>\n' % nb
+            o += '<text class="foot" x="240" y="360">%05d transactions</text>\n' % int(d['NB_TR'])
+            o += '<text class="foot" x="460" y="360">volume: %09.2f ⊔</text>\n' % su
+            o += '<text class="foot" x="680" y="360" title="number of registered Intangible Goods">%04d IG</text>\n' % (0)
+            o += '<a xlink:href="bank?source"><text class="note" text-anchor="start" x="220" y="98%" title="hack it, share it!">Download the source code!</text></a>\n'
             o += '<a xlink:href="u?pi"><text class="note" x="99%" y="20" title="at home!">Host</text></a>\n'            
-            o += '<a xlink:href="bank?log"><text class="note" x="99%" y="40">log</text></a>\n'
-            o += '<a xlink:href="bank?help"><text class="note" x="99%" y="60">help</text></a>\n'
+            o += '<a xlink:href="bank?log"><text class="note" x="99%" y="40">Log</text></a>\n'
+            o += '<a xlink:href="bank?help"><text class="note" x="99%" y="60">Help</text></a>\n'
 
-            o += '<text class="note" x="99%%" y="90%%">status: %s</text>\n' % ('ok' if (abs(ck) <= 0.00001) else 'error')
-            o += '<text class="note" x="99%%" y="94%%">digest: %s|%s</text>\n' % (d['__DIGEST__'].decode('utf-8'), __digest__.decode('utf-8'))
+            o += '<text class="note" x="99%%" y="90%%">Status: %s</text>\n' % ('ok' if (abs(ck) <= 0.00001) else 'error')
+            o += '<text class="note" x="99%%" y="94%%">Digest: %s|%s</text>\n' % (d['__DIGEST__'].decode('utf-8'), __digest__.decode('utf-8'))
             o += '<text class="note" x="99%" y="98%" title="or visit \'www.cupfoundation.net\'">Contact: laurent.fournier@cupfoundation.net</text>\n' 
             d.close()
             o += '</svg>\n'
@@ -243,7 +337,7 @@ def decrypt(d, n, raw):
     aes2 = AES.new(bytes.fromhex(c), AES.MODE_ECB)
     return aes2.decrypt(cmsg)[:lmsg]
 
-def read_ballance(owner, verbose, host='localhost'):
+def read_balance(owner, verbose, host='localhost'):
     "_"
     co = http.client.HTTPConnection(host)
     ds = dbm.open('/u/sk')    
@@ -254,7 +348,7 @@ def read_ballance(owner, verbose, host='localhost'):
     ds.close()
     return co.getresponse().read().decode('utf-8')
 
-def register(owner, iduser=None, host='localhost'):
+def register_old(owner, iduser=None, host='localhost'):
     "_"
     co = http.client.HTTPConnection(host)
     td = '%s' % datetime.datetime.now()
@@ -305,11 +399,12 @@ def buy(byr, ig, host='localhost'):
 
 if __name__ == '__main__':
     popu = {
-        'Alice':   'FR16707028009', 
-        'Valérie': 'FR16407028709', 
-        'Bob':     'FR26107028009', 
-        'Carl⊔':   'FR17707028409', 
-        'Daniel':  'FR28707028012',
+        'Pelinquin': 'FR167071927202809', 
+        'Alice':     'FR167070280099999', 
+        'Valérie':   'FR164070287098888', 
+        'Bob':       'FR261070280095555', 
+        'Carl⊔':     'FR177070284098888', 
+        'Daniel':    'FR287070280123333',
         }
     
     ds = dbm.open('/u/sk', 'c')
@@ -321,16 +416,35 @@ if __name__ == '__main__':
     ds.close()
 
     host = 'localhost'
-    for a in popu: print (register(a, popu[a], host))
-    print (register_ig('Alice', 'toto您tata', 0.56, 100000, host))
-    print (transaction('Alice',   'Bob', 1.65, host))
-    print (transaction('Valérie', 'Carl⊔', 4.65, host))
-    print (transaction('Bob',     'Valérie', 2.15, host))
-    print (transaction('Bob',     'Daniel', 4.65, host))
+    #host = 'pi.pelinquin.fr'
+    #for a in popu: print (register(a, popu[a], host))
+    #print (register_ig('Alice', 'toto您tata', 0.56, 100000, host))
+    #print (transaction('Alice',   'Bob', 1.65, host))
+    #print (transaction('Valérie', 'Carl⊔', 4.65, host))
+    #print (transaction('Bob',     'Valérie', 2.15, host))
+    #print (transaction('Bob',     'Daniel', 4.65, host))
     #print (buy('Bob', 'toto您tata', host))
-    print (read_ballance('Daniel', False, host))
-    print (transaction('Daniel', 'Valérie', 44.65, host))
-    print (read_ballance('Valérie', False, host))
+    #print (read_balance('Daniel', False, host))
+    #print (transaction('Daniel', 'Valérie', 44.65, host))
+    #print (read_balance('Valérie', False, host))
+
+    co = http.client.HTTPConnection(host)
+    ds = dbm.open('/u/sk')    
+    ki = [b64toi(x) for x in ds['Alice'].split()]
+    kb = [x for x in ds['Alice'].split()]
+    ds.close()
+    s = sign(ki[1], ki[2], 'blabla')
+
+    cmd = 'TEST ( "hello Valérie", "%s")' % s.decode('ascii')
+    co.request('POST', '/bank', urllib.parse.quote(cmd))
+    print (co.getresponse().read().decode('utf-8'))
+    co.request('GET', '/bank?' + urllib.parse.quote(cmd))
+    print (co.getresponse().read().decode('utf-8'))
+
+
+    print (register('Valérie', 'anonymous', False, 'localhost'))
+
+
     
     sys.exit()
 
